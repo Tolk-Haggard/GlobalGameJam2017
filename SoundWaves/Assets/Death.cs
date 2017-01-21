af42b66e8e1dd4af0c2f5f0de624195756cc9ef0
Lois Haggard <gabehagg@gmail.com>
2017-01-21 17:28:25 -0600

    added player death

---
+++ SoundWaves/Assets/Death.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col) { 
+		if (col.gameObject.name.Contains ("Monster")) {
+			Debug.Log ("should be dying here");
+			SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+		}
+	}
+}
