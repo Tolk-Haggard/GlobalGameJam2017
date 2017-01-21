7c659acedee14026969f64b85868bd5052667d18
Lois Haggard <gabehagg@gmail.com>
2017-01-21 16:58:46 -0600

    added DeastroyEnemy script

---
+++ SoundWaves/Assets/DestroyEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemy : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name.Contains("Monster"))
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
