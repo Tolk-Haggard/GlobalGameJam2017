d1e26b4d54ab6f63070dbb90803f641962ba85db
Lois Haggard <gabehagg@gmail.com>
2017-01-21 16:42:03 -0600

    Added Monster spawn, and movement

---
+++ SoundWaves/Assets/EnemyAI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+	private GameObject camera;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.Find ("Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, camera.transform.position, speed * Time.deltaTime);	
+	}
+}
---
+++ SoundWaves/Assets/SpawnEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour {
+
+	public float timer;
+	public GameObject prefab;
+	public float positionX;
+	public float positionY; 
+	public float positionZ; 
+	private float tempTimer;
+
+
+	void Start() {
+		tempTimer = timer;
+	}
+
+
+	void Update () {
+
+		tempTimer -= Time.deltaTime;
+
+		if (tempTimer <= 0) {
+			Instantiate (prefab,new Vector3(positionX, positionY, positionZ), transform.rotation); 
+			tempTimer = timer;
+		}
+		
+	}
+}
