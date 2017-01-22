52b47e0fd09908a8c2654c93793c37c0cbb43a87
Brian Haggard <brihagg@gmail.com>
2017-01-22 01:11:51 -0600

    Yell to start! - BH

--- SoundWaves/Assets/PlayGame.cs
+++ SoundWaves/Assets/PlayGame.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
 public class PlayGame : MonoBehaviour {
 
-	// Use this for initialization
+	private AudioSource aud;
+	private float[] spectrumData;
+
+	public float threshold;
+
 	void Start () {
-		
+		aud = GetComponent<AudioSource>();
+		aud.clip = Microphone.Start(null, true, 1, 11025);
+		aud.loop = true;
+		aud.mute = false;
+		aud.Play();
+		spectrumData = new float[64];
 	}
-	
 
 	void Update () {
-	
-		if (Input.GetKeyDown("space"))
+		var magnitude = 0.0f;
+		aud.GetOutputData (spectrumData, 0);
+
+		for (var j = 0; j < spectrumData.Length; j++) {
+			magnitude += spectrumData [j];
+		}
+
+		magnitude = (float)System.Math.Round (magnitude, 2);
+
+		if (magnitude > threshold) {
 			SceneManager.LoadScene ("OpeningMovie", LoadSceneMode.Single);
+		}
 	}
 }
